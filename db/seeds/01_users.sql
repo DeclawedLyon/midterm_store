1d0730bf0f3cb5fe3cf81b2749af11dc9b191a79
andysongsong <sxj930916@gmail.com>
2021-08-21 18:01:19 +0000

    add table

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,18 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email,password)
+VALUES ('Alice','aaaaaa@yahoo.com', '111111');
+INSERT INTO users (name,email,password)
+VALUES ('Kira','bbbbbb@gmail.com', '22222');
+INSERT INTO users (name,email,password)
+VALUES ('Mila','cccccc@outlook.com', '33333');
+INSERT INTO users (name,email,password)
+VALUES ('Declan','dddddd@ball.com', '55555');
+INSERT INTO users (name,email,password)
+VALUES ('Andy','eeeeee@live.com', '44444');
+INSERT INTO users (name,email,password)
+VALUES ('Tom','ffffff@hot.com', '66666');
+
+
+INSERT INTO books (owner_id,price,author,title,genre,year,bookcover)
+VALUES()
+
