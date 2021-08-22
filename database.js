1048c389f0d86ff7eebb1ced93e673c29b618e54
DeclawedLyon <declan.e.lyons@gmail.com>
2021-08-21 18:58:37 -0600

    implemented get all books javascript query

--- database.js
+++ database.js
@@ -5,13 +5,54 @@ const pool = new Pool({
   database: 'midterm'
 });
 
-const findAllBooks = function() {
+const getAllBooks = function(limit) {
   return pool
   .query(`
     SELECT *
     FROM books
-    `)
+    LIMIT $1
+    `, [limit])
   .then((result) => result.rows[0])
   .catch((err) => err.message);
 }
-exports.findAllBooks = findAllBooks;
+exports.getAllBooks = getAllBooks;
+
+const getAllFavorites = function(userId) {
+  return pool
+  .query(`
+  SELECT favorites.id, users.name, books.title
+  FROM favorites
+  JOIN users ON user_id = users.id
+  JOIN books ON book_id = books.id
+  WHERE name = 'Mila';
+`)
+.then((result) => result.rows[0])
+.catch((err) => err.message);
+}
+
+const getUserWithEmail = function(email) {
+  return pool
+  .query(`
+    SELECT *
+    FROM users
+    WHERE email = $1
+    `, [email])
+  .then((result) => result.rows[0])
+  .catch((err) => err.message);
+}
+exports.getUserWithEmail = getUserWithEmail;
+
+const addUser =  function(user) {
+  const name = user.name;
+  const email = user.email;
+  const password = user.password;
+  return pool
+    .query(`
+      INSERT INTO users (name, email, password)
+      VALUES ($1, $2, $3)
+      RETURNING *
+      `, [name, email, password])
+    .then((result) => result.rows)
+    .catch((err) => err.message);
+}
+exports.addUser = addUser;
