18e678b598a5683edf72c4503b462067d22a3d24
Lyudmila Nevedomskaya <milo4kamoya@gmail.com>
2021-08-25 16:23:55 +0000

    Changed add.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,7 @@ $(() => {
     url: "/api/users",
   }).done((users) => {
     for (user of users.users) {
-      $("<div>").text(user.name).appendTo($("body"));
+      //$("<div>").text(user.name).appendTo($("body"));
     }
   });
 
