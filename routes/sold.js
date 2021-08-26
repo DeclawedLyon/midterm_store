ba17c9cc0d87a841a51db5f14892fbc5852b0699
andysongsong <sxj930916@gmail.com>
2021-08-26 03:06:24 +0000

    no change

--- routes/sold.js
+++ routes/sold.js
@@ -9,7 +9,6 @@ router.get("/sold/:id", (req, res) => {
     .soldBook(bookid)
     .then((data) => {
       const templeteVars = { data };
-      console.log("dadadadadadada1213", data);
       templeteVars.user = req.session.user_id ? req.session.user_id : null;
       res.render("mybooks", templeteVars);
     })
@@ -18,4 +17,18 @@ router.get("/sold/:id", (req, res) => {
     });
 });
 
+router.post("/delete/:id", (req, res) => {
+  const bookid = req.params.id;
+
+  database
+    .removeBook(bookid)
+    .then((data) => {
+      const templeteVars = { data };
+      templeteVars.user = req.session.user_id ? req.session.user_id : null;
+      res.render("mybooks", templeteVars);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
 module.exports = router;
